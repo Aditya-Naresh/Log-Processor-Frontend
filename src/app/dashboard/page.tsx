24b65aea1b63a18a4d40d1a230352f7e20355a41
Aditya Naresh <adityanaresh007@gmail.com>
2025-03-25 11:11:36 +0530

    Supabase authentication set up pending to store the tokens in the redux

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import DashBoard from '../../components/Dashboard.tsx'
+const page = () => {
+  return (
+    <div> <DashBoard /></div>
+  )
+}
+
+export default page
---
+++ src/components/GoogleAuth.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import {supabase} from "../helper/supabaseClient.ts";
+import { useNavigate } from "react-router-dom";
+import {useRouter} from "next/navigation"
+
+const GoogleAuth = () => {
+    const router = useRouter()
+    useEffect(() => {
+        // Check for an existing session on mount
+        const checkSession = async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            if (session) {
+                console.log(session);
+                router.push("/dashboard");
+            }
+        };
+
+        checkSession();
+
+        // Listen for authentication state changes
+        const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+            if (session) {
+                console.log(session);
+                router.push("/dashboard");
+                
+            } else {
+                router.push("/login");
+            }
+        });
+
+        return () => authListener?.subscription?.unsubscribe(); // Ensure safe cleanup
+    }, []);
+
+    return (
+        <div>
+            <Auth
+                supabaseClient={supabase}
+                providers={["google"]}
+                socialLayout="horizontal"
+                socialButtonSize="small"
+                appearance={{ theme: ThemeSupa }}
+            />
+        </div>
+    );
+};
+
+export default GoogleAuth;
+
